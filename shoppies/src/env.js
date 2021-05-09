e3afe5a7f76bbad854c99364d4fe725a47a09605
Jefferson(Zhe Fan) Li <jeffersonli.li@mail.utoronto.ca>
2021-05-08 20:13:04 -0400

    Update env.js

--- shoppies/src/env.js
+++ shoppies/src/env.js
@@ -1,2 +1,3 @@
 const apiKey = `6bc0eaa3`;
 export const apiURL = `https://www.omdbapi.com/?apikey=${apiKey}`;
+// because my api key is attached to every request anyways, i feel comfortable exposing this file.
